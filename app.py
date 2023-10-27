d916ca49e8a58f7105d69a2f1d3432143b1d9b48
Vladimir <kusakinvova@gmail.com>
2023-10-27 13:40:45 +0300

    feat: add route for items

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import requests
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 # Функция для получения содержимого страницы Hacker News
 
-def get_hacker_news_page():
-    url = "https://news.ycombinator.com/"
+def get_hacker_news_page(url):
     response = requests.get(url)
     return response.text
 
@@ -26,9 +25,22 @@ def modify_text(text):
 # Маршрут для обработки запросов
 @app.route('/')
 def hacker_news_proxy():
-    hacker_news_page = get_hacker_news_page()
+    url = "https://news.ycombinator.com/"
+    hacker_news_page = get_hacker_news_page(url)
     modified_page = modify_text(hacker_news_page)
     return Response(modified_page, content_type='text/html; charset=utf-8')
+
+# Маршрут для обработки внутренних страниц
+@app.route('/item')
+def hacker_news_item():
+    item_id = request.args.get('id')
+    if item_id:
+        url = f"https://news.ycombinator.com/item?id={item_id}"
+        item_page = get_hacker_news_page(url)
+        modified_page = modify_text(item_page)
+        return Response(modified_page, content_type='text/html; charset=utf-8')
+    else:
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8232)
